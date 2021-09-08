59011b2a48ee7d14bf41d466b6fd93dc8e4068b8
Shreya Rajput <89004601+shreyarajput1@users.noreply.github.com>
2021-09-08 15:43:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Login from './pages/Login';
+import from 'react';
 
-
-import SignUp from './pages/signup';
-import Contact from './pages/contact';
-  
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/home' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route path="/login" component={Login}/>
-        <Route path='/sign-up' component={SignUp} />
-      </Switch>
-    </Router>
-  );
-}
-  
-export default App;
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
